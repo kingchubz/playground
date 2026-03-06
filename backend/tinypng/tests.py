3dee5927365488986ff5ad8db1504681f4dcc8ee
Sviatoslav Dmytriiev <shinyhero777@gmail.com>
2026-03-06 11:54:16 +0100

    refactor tests

--- backend/tinypng/tests.py
+++ backend/tinypng/tests.py
@@ -1,23 +1,39 @@
 from datetime import datetime, timedelta
-import json
-import os
 
+from django.utils import timezone
 from django.test import TestCase, Client
 
-client = Client()
 
-class DatetimeTest(TestCase):
+class TestDatetime(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = '/api/time/'
+
+    def test_time_returns_200(self):
+        """
+        curr_date() response status code = 200
+        """
+
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_time_wrong_url(self):
+        """
+        curr_date() response status code = 404
+        """
+
+        response = self.client.get('/api/a') # wrong url
+        self.assertEqual(response.status_code, 404)
+
     def test_time_is_correct(self):
         """
         curr_date() returns correct time within 1 second.
         """
-        allowed_hosts = os.environ['ALLOWED_HOSTS'].split('|')
 
-        for host in allowed_hosts:
-            response = client.get('/api/time/', headers={'Host': host})
-            self.assertEqual(response.status_code, 200)
+        response = self.client.get(self.url)
+        data = response.json()
+        self.assertIn('datetime', data)
 
-            curr_date_dict = json.loads(response.getvalue())
-            curr_datetime = datetime.strptime(curr_date_dict['datetime'], '%a %d %b %Y %H:%M:%S')
-            diff = datetime.now() - curr_datetime
-            self.assertIs(diff <= timedelta(seconds=1), True)
+        curr_datetime = datetime.fromisoformat(data['datetime'])
+        diff = timezone.now() - curr_datetime
+        self.assertTrue(diff <= timedelta(seconds=1))
